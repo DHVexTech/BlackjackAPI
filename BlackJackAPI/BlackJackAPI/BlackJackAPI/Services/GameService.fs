c9123f31de8793a6e163b28779126f2eafc48f6d
VexTech <herve@intechinfo.fr>
2020-06-05 18:03:16 +0200

    Start Calculate Method

--- BlackJackAPI/BlackJackAPI/BlackJackAPI/Services/GameService.fs
+++ BlackJackAPI/BlackJackAPI/BlackJackAPI/Services/GameService.fs
@@ -103,61 +103,23 @@ module GameService =
             item
 
         
-
-    //let id = 1
-    //let State = Created
-    // enum 2 joueurs
-    // objet contient 2 joueurs tableau avec carte en main avec statut
-    //    let state = "Created"
-
-    //return
-
-    //let PickCard :
-
-
-
-   //createGame(2id joeuurs et renvoie id a partir du front + deck + state)
-   //pickCard
-   //etat
-
-   //cas où ça dépasse 21
-
-
-
-   //doturn (requetes http?/validation)
-   //result (after turn)
-
-
-   (*
-    let CreateDeck : Card[] (*: Card[]*) = 
-        let names = Enum.GetNames(typeof<CardsNames>)
-        let symbols = Enum.GetNames(typeof<CardsSymbols>)
-        let resizeArrayCards = ResizeArray<Card>()
-
-        let CreateCard = fun (symbol : string, name : string) ->
-            resizeArrayCards.Add {
-                Name = name
-                Symbol = symbol
-                FirstValue = 0
-                SecondValue = 0
-            }
+    //let CountScore(item:Game) : Game =
+    //    let mutable playerOneScoreNumber = 0
+    //    let rec loopingCardsPlayerOne'(count:int) =
+    //        match item.PlayerOneHand.[count] with
+    //        | card when count < item.PlayerOneHand.Length ->
+    //            playerOneScoreNumber = playerOneScoreNumber + card.FirstValue
+    //            match card.SecondValue with
+    //            | value when value <> 0 ->
+    //            | _ -> ignore()
+    //        | card when (count+1) = item.PlayerOneHand.Length ->
+    //        | _ -> ignore()
+    //        ""
         
-        let rec loopingNames' (countName : int) =
-            let rec loopingSymbols' = fun (countSymbol : int, name : string) ->
-                match symbols.[countSymbol] with
-                | value when (countSymbol+1) = symbols.Length -> CreateCard (value, name)
-                | value when countSymbol < symbols.Length -> 
-                    CreateCard (value, name)
-                    loopingSymbols' (countSymbol+1, name)
+    //    let rec loopingCardsPlayerTwo'(count:int) : string =
+    //        ""
+        
+    //    let playerOneScore = loopingCardsPlayerOne'(0)
+    //    let playerTwoScore = loopingCardsPlayerTwo'(0)
 
-                
-            match names.[countName] with
-            | value when (countName+1) = names.Length -> loopingSymbols' (0, value)
-            | value when countName < names.Length -> 
-                loopingSymbols' (0, value)
-                loopingNames' (countName+1)
-                
-        let countName = 0
-        loopingNames' (countName)
-        resizeArrayCards.ToArray()
-     *)
+    //    item
